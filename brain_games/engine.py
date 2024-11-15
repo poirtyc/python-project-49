c43125a2c344318acab7861af7a2c7e7b9a6e042
Dmitriy Boyko <Dmitriyboyko19@gmail.com>
2024-11-15 21:24:43 +0500

    fixed magic number in engin

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -7,7 +7,7 @@ def general_logic(RULES, generate_round):
     player_name = welcome_user()
     print(RULES)
 
-    diversity_rounds = 4
+    diversity_rounds = 3
     
     for _ in range(diversity_rounds):
         question, answer = generate_round()
